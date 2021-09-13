2ac595b694992b7439c92ae275ed3a3234730546
tusharrastogi17 <68027060+tusharrastogi17@users.noreply.github.com>
2021-09-13 15:04:43 +0530

    24 ms

--- solutions/203. Remove Linked List Elements.cpp
+++ solutions/203. Remove Linked List Elements.cpp
@@ -1,21 +1,22 @@
-        ListNode* temp=head;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
         
-       /* cout<<"headi "<<head<<endl;
-        while (head != NULL) {
-            cout<< head->val <<" "<<head->next<<" ";
-            head = head->next;
-        }
-        cout<<endl<<"prev  "<<prev<<endl;
-        while (prev != NULL) {
-            cout<< prev->val <<" "<<prev->next<<" ";
-            prev = prev->next;
-        }
-        cout<<endl<<"temp "<<temp<<endl;
-          while (temp != NULL) {
-            cout<< temp->val <<" "<<temp->next<<" ";
-            temp = temp->next;
-        }
-        cout<<endl<<"end";*/
+        if(head == NULL){
+            return NULL;
+        }
+        ListNode* prev=head;
+        ListNode* temp=head;
         
         while(temp!=NULL){
             if(head->val==val){
@@ -26,9 +27,10 @@
                 temp=temp->next;
                 continue;
             }
-            prev=temp;
-            temp=temp->next;
-            
+           
+                prev=temp;
+                temp=temp->next;
+             
         }
         return head;
     }
