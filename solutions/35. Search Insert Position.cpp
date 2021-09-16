4fd9fb86d1a5bf73516c953779e2473ad4176042
tusharrastogi17 <68027060+tusharrastogi17@users.noreply.github.com>
2021-09-17 03:25:41 +0530

    4 ms

---
+++ solutions/35. Search Insert Position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int r=0;
+        if(target==0){
+            return r;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target){
+                r=i;
+                break;
+               cout<<i;
+            }
+            if(nums[i]<target){
+                r=i+1;
+            }
+        }
+        return r;
+        
+    }
+};
